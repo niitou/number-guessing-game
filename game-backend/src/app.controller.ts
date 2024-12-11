4432091f44a6b8b6fe90fa25cf3e69402385eec1
noel <noelnolife@gmail.com>
2024-12-11 12:20:08 +0700

    add secret number generator service

--- game-backend/src/app.controller.ts
+++ game-backend/src/app.controller.ts
@@ -9,4 +9,15 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  // Need to change to post and check if player guesses is lower, higher, or same as the secret number
+  @Get('/game')
+  game() {
+    return this.appService.secret_number;
+  }
+
+  @Get('/new')
+  newNumber() {
+    return this.appService.getNewNumber();
+  }
 }
--- game-backend/src/app.service.ts
+++ game-backend/src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  // declare here so the number won't change everytime a request is made
+  secret_number = Math.floor(Math.random() * 1000) + 1;
   getHello(): string {
     return 'Hello World!';
+  }
+
+  // get new number in case user wants to play another game
+  getNewNumber(): Number {
+    return (this.secret_number = Math.floor(Math.random() * 1000) + 1);
   }
 }
