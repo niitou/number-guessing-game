4e5cfc20cd572195193324eb292408b75b3d4909
noel <noelnolife@gmail.com>
2024-12-12 11:01:18 +0700

    add post request

--- game-frontend/src/component/InputComponent.tsx
+++ game-frontend/src/component/InputComponent.tsx
@@ -2,15 +2,25 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { increment } from '../reducers/userScoreReducer'
 import { change } from '../reducers/userInputReducer'
+import axios from 'axios'
 
 function InputComponent() {
-  const userInput = useSelector((state:any) => state.input.value)
+  const userInput = useSelector((state: any) => state.input.value)
   const dispatch = useDispatch()
+    // const playerId = useSelector((state : any) => state.player.id)
+  const handleSubmit = ( e : any ) =>{
+    e.preventDefault();
+    axios.post(`${process.env.REACT_APP_BACKEND_URL}/game`, {
+      data : userInput
+    })
+  }
   return (
     <div>
       <div>
-        <input type="text" name="user-input" value={userInput} onChange={(e) => dispatch(change(e.target.value))}/>
-        <button onClick={() => dispatch(increment())}>Submit</button>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="user-input" value={userInput} onChange={(e) => dispatch(change(e.target.value))} />
+          <button onClick={() => dispatch(increment())}>Submit</button>
+        </form>
       </div>
     </div>
   )
