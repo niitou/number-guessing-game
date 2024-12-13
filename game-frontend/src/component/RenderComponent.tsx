aa2c04fb2f3df032619f426946a16ec0b94c200c
noel <noelnolife@gmail.com>
2024-12-13 12:48:30 +0700

    add functionality to yes and no button

--- game-frontend/src/component/RenderComponent.tsx
+++ game-frontend/src/component/RenderComponent.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { resetScore } from '../reducers/userScoreReducer'
+import { setStatus } from '../reducers/gameReducer'
+import { setPlayerInput } from '../reducers/userInputReducer'
+// import { setPlayer } from '../reducers/playerNameReducer'
+import axios from 'axios'
 
 function RenderComponent() {
   const playerScore = useSelector((state: any) => state.score.value)
   const message = useSelector((state: any) => state.message.value)
   const gameStatus = useSelector((state: any) => state.game.status)
   const gameMessage = useSelector((state: any) => state.game.message)
+  const dispatch = useDispatch()
+
+  const newGame = () => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/new`) // get request to generate new secret number
+    dispatch(resetScore()) // Set score to 0
+    dispatch(setStatus({ // Set game status back to initial value
+      message: '',
+      status: 'ONGOING'
+    }))
+    dispatch(setPlayerInput('')) // Set user input to initial value
+  }
+
+  const gameOver = () => {
+    // newGame()
+    // dispatch(setPlayer({
+    //   id: 0,
+    //   value: ""
+    // }))
+    window.location.reload()
+  }
   return (
     <div>
       <p>{message}</p>
@@ -18,8 +43,8 @@ function RenderComponent() {
           <div>
             <p>Play Again ?</p>
             <div>
-              <button>Yes</button>
-              <button>No</button>
+              <button onClick={newGame}>Yes</button>
+              <button onClick={gameOver}>No</button>
             </div>
           </div>
       }
