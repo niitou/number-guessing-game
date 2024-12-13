55845a75497613a691ea485535dd35360e07bf39
noel <noelnolife@gmail.com>
2024-12-13 12:36:07 +0700

    add conditional rendering when the game is finish

--- game-frontend/src/component/RenderComponent.tsx
+++ game-frontend/src/component/RenderComponent.tsx
@@ -2,14 +2,27 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 function RenderComponent() {
-  const playerScore = useSelector((state : any) => state.score.value)
-  const playerInput = useSelector((state: any) => state.input.value)
-  const message = useSelector((state:any) => state.message.value)
+  const playerScore = useSelector((state: any) => state.score.value)
+  const message = useSelector((state: any) => state.message.value)
+  const gameStatus = useSelector((state: any) => state.game.status)
+  const gameMessage = useSelector((state: any) => state.game.message)
   return (
     <div>
       <p>{message}</p>
-      <p>Current score : <span>{playerScore}</span></p>
-      <h1>Guess number : {playerInput}</h1>
+      {gameMessage === "" ? <></> : <h2>{gameMessage}</h2>}
+      {
+        gameStatus === "ONGOING" ?
+          <div>
+            <p>Current score : <span>{playerScore}</span></p>
+          </div> :
+          <div>
+            <p>Play Again ?</p>
+            <div>
+              <button>Yes</button>
+              <button>No</button>
+            </div>
+          </div>
+      }
     </div>
   )
 }
